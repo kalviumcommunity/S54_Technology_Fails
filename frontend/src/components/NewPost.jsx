26711c6be941fdad9f6e3d4d39ba53431b138783
RikhilTaneja <rikhiltaneja@gmail.com>
2024-02-15 09:57:35 +0530

    Loading functionality added in new-form

--- frontend/src/components/NewPost.jsx
+++ frontend/src/components/NewPost.jsx
@@ -1,20 +1,14 @@
 import React from "react";
 import { useForm } from "react-hook-form";
-import {useNavigate} from "react-router-dom"
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import {
-  FormControl,
-  FormLabel,
-  Input,
-  Text,
-  Button
-} from "@chakra-ui/react";
-import {Link} from 'react-router-dom'
-import axios from "axios"
+import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { FormControl, FormLabel, Input, Text, Button } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 export default function NewPost() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const {
     register,
     handleSubmit,
@@ -23,46 +17,73 @@ export default function NewPost() {
     formState: { errors },
   } = useForm();
   // console.log(watch())
-  const FormSubmitHandler = (data)=>{
-    axios.post("https://technology-fails.onrender.com/posts",data).then(()=>{
-      console.log("ADDED")
-      navigate("/listings")
-    }).catch((err)=>{
-      console.log(err)
-      toast.error("Some error occurred.")
-    })
-  }
+  const FormSubmitHandler = (data) => {
+    const id = toast.loading("Adding");
+    setTimeout(() => {
+      axios
+        .post("https://technology-fails.onrender.com/posts", data)
+        .then(() => {
+          console.log("ADDED");
+          toast.update(id, {
+            render: "Added",
+            type: "success",
+            isLoading: false,
+          });
+          setTimeout(() => {
+            navigate("/listings");
+          }, 1200);
+        })
+        .catch((err) => {
+          console.log(err);
+          toast.update(id, {
+            render: "Some error occurred",
+            type: "error",
+            isLoading: false,
+          });
+        });
+    }, 1000);
+  };
   return (
     <div className="form-parent">
       <ToastContainer />
       <form className="form" onSubmit={handleSubmit(FormSubmitHandler)}>
-        <Text as="b" fontSize="2.3vmax">New Post</Text>
-        <Text as="i" fontSize="1vmax">Enter the following details!</Text>
+        <Text as="b" fontSize="2.3vmax">
+          New Post
+        </Text>
+        <Text as="i" fontSize="1vmax">
+          Enter the following details!
+        </Text>
         <FormControl>
-          <FormLabel fontSize="1.2vmax" as="i" fontWeight="550">Username</FormLabel>
+          <FormLabel fontSize="1.2vmax" as="i" fontWeight="550">
+            Username
+          </FormLabel>
           <Input
             type="text"
             borderColor="black"
             {...register("user", {
-              required: "Username is required"
+              required: "Username is required",
             })}
           />
           <p className="err">{errors.user?.message}</p>
         </FormControl>
         <FormControl>
-          <FormLabel fontSize="1.2vmax" as="i" fontWeight="550">Title</FormLabel>
+          <FormLabel fontSize="1.2vmax" as="i" fontWeight="550">
+            Title
+          </FormLabel>
           <Input
             type="text"
             borderColor="black"
             {...register("title", {
               required: "Title is required",
-              maxLength:{ value: 40, message: "Max 40 Chars" }
+              maxLength: { value: 40, message: "Max 40 Chars" },
             })}
           />
           <p className="err">{errors.title?.message}</p>
         </FormControl>
         <FormControl>
-          <FormLabel fontSize="1.2vmax" as="i" fontWeight="550">Tagline</FormLabel>
+          <FormLabel fontSize="1.2vmax" as="i" fontWeight="550">
+            Tagline
+          </FormLabel>
           <Input
             type="text"
             borderColor="black"
@@ -73,7 +94,9 @@ export default function NewPost() {
           <p className="err">{errors.tagline?.message}</p>
         </FormControl>
         <FormControl>
-          <FormLabel fontSize="1.2vmax" as="i" fontWeight="550">Image Link</FormLabel>
+          <FormLabel fontSize="1.2vmax" as="i" fontWeight="550">
+            Image Link
+          </FormLabel>
           <Input
             type="text"
             borderColor="black"
@@ -83,9 +106,10 @@ export default function NewPost() {
           />
           <p className="err">{errors.image?.message}</p>
         </FormControl>
-        <Button type="submit" colorScheme="red">Submit</Button>
+        <Button type="submit" colorScheme="red">
+          Submit
+        </Button>
       </form>
     </div>
   );
 }
-
